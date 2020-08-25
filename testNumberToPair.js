2cc2cf083a0c6f92f49a2795a4cae0f1873e68fd
harishtrv <61978004+harishtrv@users.noreply.github.com>
2020-08-25 13:52:03 +0530

    Add files via upload

---
+++ testNumberToPair.js
@@ -0,0 +1,9 @@
+const expect = require('chai').expect;
+var GetColorFromPairNumber=require("./GetColorFromPairNumber");
+function testNumberToPair(number, expectedMajor, expectedMinor) {
+  const pairOfColors = GetColorFromPairNumber(number);
+  console.log(`${number} = ${expectedMajor} ${expectedMinor}`);
+  expect(pairOfColors.major).equals(expectedMajor);
+  expect(pairOfColors.minor).equals(expectedMinor);
+}
+module.exports=testNumberToPair;
